b59e4cf1cb34c4611bfde011089884efd42cb9e6
miyauchi <miyauchi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-28 11:53:18 +0000

    first commit
    git-svn-id: https://plugins.svn.wordpress.org/oembed-gist/trunk@378346 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ oembed_gist.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Plugin Name: oEmbed gist
+Plugin URI: http://firegoby.theta.ne.jp/wp/oembed-gist
+Description: Embed source from gist.github.
+Author: Takayuki Miyauchi (THETA NETWORKS Co,.Ltd)
+Version: 0.1.0
+Author URI: http://firegoby.theta.ne.jp/
+*/
+
+/*
+Copyright (c) 2010 Takayuki Miyauchi (THETA NETWORKS Co,.Ltd).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+new gist();
+
+class gist {
+
+    private $html = '<script src="https://gist.github.com/%s.js"></script>';
+
+    function __construct()
+    {
+        wp_embed_register_handler(
+            'gist',
+            '#https://gist.github.com/([0-9]+)#i',
+            array(&$this, 'handler')
+        );
+        add_shortcode('gist', array(&$this, 'shortcode'));
+        add_filter('plugin_row_meta', array(&$this, 'plugin_row_meta'), 10, 2);
+    }
+
+    public function handler($m, $attr, $url, $rattr)
+    {
+        return '[gist id="'.$m[1].'"]';
+    }
+
+    public function shortcode($p)
+    {
+        if (preg_match("/^[0-9]+$/", $p['id'])) {
+            return sprintf($this->html, $p['id']);
+        }
+    }
+
+    public function plugin_row_meta($links, $file)
+    {
+        $pname = plugin_basename(__FILE__);
+        if ($pname === $file) {
+            $links[] = '<a href="http://firegoby.theta.ne.jp/pfj/">Pray for Japan</a>';
+        }
+        return $links;
+    }
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== url2link ===
+Contributors: miyauchi
+Donate link: http://firegoby.theta.ne.jp/
+Tags: url, link, oembed, visual editor, post, gist, github
+Requires at least: 3.0
+Tested up to: 3.1
+Stable tag: 0.1.0
+
+Embed your source from gist into WordPress easily.
+
+== Description ==
+
+Embed your source from gist into WordPress easily.
+You simply paste URL of your Gist.
+
+
+= Support: =
+
+* [Plugin Homepage](http://firegoby.theta.ne.jp/wp/oembed-gist)
+* [Support](http://wordpress.org/tags/oembed-gist)
+
+
+== Installation ==
+
+* A plug-in installation screen is displayed on the WordPress admin panel.
+* It installs it in `wp-content/plugins`.
+* The plug-in is made effective.
+
+
+== Screenshots ==
+
+1. Copy URL from gist.github.com.
+1. You simply paste URL into WordPress.
+1. It just appears!
+
+== Changelog ==
+
+= 0.1.0 =
+* The first release.
+
+== Credits ==
+
+This plug-in is not guaranteed though the user of WordPress can freely use this plug-in free of charge regardless of the purpose.
+The author must acknowledge the thing that the operation guarantee and the support in this plug-in use are not done at all beforehand.
+
+== Contact ==
+
+* email to miya[at]theta.ne.jp
+* twitter @miya0001
